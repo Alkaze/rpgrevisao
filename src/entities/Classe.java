6c209366277ae01db2477c90cb4deeff5bc45efd
Lucas Lopes <lucas.llopes99@gmail.com>
2020-06-20 22:30:18 -0300

    Inicio de desenvolvimento das classes Habilidade e Classe

---
+++ src/entities/Classe.java
@@ -0,0 +1,20 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Classe {
+	
+	private List<Habilidade> listaHabilidades = new ArrayList<>();
+
+	public List<Habilidade> getListaHabilidades() {
+		return listaHabilidades;
+	}
+
+	public void setListaHabilidades(List<Habilidade> listaHabilidades) {
+		this.listaHabilidades = listaHabilidades;
+	}
+	
+	
+
+}
